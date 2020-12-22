7bf94612d6299ac5ead52e946a591c5edbec4af6
Yulevin <yulevinpost@gmail.com>
2020-12-23 00:43:41 +0300

    added ajax, api, wRouter, params, cookies, login, auth, follow-unfollow api

--- react-samurai/src/components/Header/Header.jsx
+++ react-samurai/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import styles from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={styles.header}>
         <img alt='Logo' src='https://cdn.pixabay.com/photo/2016/01/19/07/35/social-1148031_960_720.png' />
+
+        <div className={styles.loginBlock}>
+            {props.isAuth
+                ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>;
 }
 
--- react-samurai/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react-samurai/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import styles from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
 const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />
+    }
     return (
         <div>
             <div>
                 <img
                     alt='pic'
-                    // src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.w3schools.com%2Fw3images%2Favatar2.png&f=1&nofb=1'
                     src='https://www.ee.ryerson.ca/css/images/header.png'
                 />
             </div>
             <div className={styles.descriptionBlock}>
-                <img
+                <img src={props.profile.photo.large}
                     alt='ava'
-                    // src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.w3schools.com%2Fw3images%2Favatar2.png&f=1&nofb=1'
                 />
                 ava + description
             </div>
